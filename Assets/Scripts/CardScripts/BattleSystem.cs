82b151bb7722109cededeec64883df0581770424
Steven <stevvenkon@gmail.com>
2022-06-07 18:05:54 -0400

    CleaningBattleSystem Script

--- Assets/Scripts/CardScripts/BattleSystem.cs
+++ Assets/Scripts/CardScripts/BattleSystem.cs
@@ -31,7 +31,7 @@ public class BattleSystem : MonoBehaviour
         cmang = GetComponent<CardGameManager>();
         playerHud = GetComponent<PlayerHUD>();
         refGrid = GetComponent<ReferenceGridObjects>();
-        
+        //Begin the combat
        SetupBattle();
     } 
 
@@ -40,65 +40,57 @@ public class BattleSystem : MonoBehaviour
     }
 
     public void SetupBattle(){
+        //Check to see if this a test or trying to import an enemy from overworld
         if (StaticEnemy.enemyToFight.Count != 0){
             pHolderenemy.RemoveAll(EnemyPredictable);
-
             for (int i = 0; i < StaticEnemy.enemyToFight.Count; i ++){
                 pHolderenemy.Add(StaticEnemy.enemyToFight[i]);
             }
         }
-
-         int manaStart = cmang.manatotal;
-        
+        //Create the enemy graphics      
         for (int i = 0 ; i < pHolderenemy.Count; i++){
             GameObject enemyObject = Instantiate(enemyPrefab, new Vector3(0,0,0), Quaternion.identity);
             enemyObject.transform.SetParent(enemyArea.transform, false);
             enemyObject.GetComponent<CardDisplay>().SetInfo(pHolderenemy[i]);
         }
-
-     
         //Temporary 
+        //TODO Add for dynamic combat sizes
         cmang.board[0, 3] = pHolderenemy[0];
         cmang.board[1, 3] = pHolderenemy[0];
         cmang.board[2, 3] = pHolderenemy[0];
-   
-        
-        
-
+        //Manages mana
         cmang.mana = cmang.manaStart;
         cmang.manatotal = cmang.manaStart;
+        //Import health from overworld
         hpbar.StartReduceHealth();
-        
+        //Begin players turn
         StartCoroutine(PlayerTurn());
     }
 
     public IEnumerator PlayerTurn(){
-        
+        //Player's turn 
         state = BattleState.START;
         cmang.manatotal += 1;
         cmang.mana = cmang.manatotal;
-
+        playerHud.SetPlayerMana(cmang.mana);
         //Draw Opening Hand
         cmang.DrawCard();
         cmang.DrawCard();
         cmang.DrawCard();
 
        
-       //Player begins!
-       state = BattleState.PLAYERTURN;
-       PlayerHUDChanges("Your turn");
-
+       
+       //GUI "Your turn"
+       playerHud.HUDTextUpdate("Your Turn");
        yield return new WaitForSecondsRealtime(1f);
-
        playerHud.StopTextTurnText();
-
 
        //Which cells is the enemy targetting? What attack will it be with?
        chosenEnemyAction = pHolderenemy[0].cActions[Random.Range(0, pHolderenemy[0].cActions.Length)];
        pHolderenemy[0].myDisplay.transform.gameObject.GetComponent<EffectDisplay>().SetEffectInfo(chosenEnemyAction);
-
+       //2D arr of bool
         arrTarget  = chosenEnemyAction.TargetSpaces(cmang.board);
-        
+        //GUI Markers for where the enemy effect will go
        for (int i = 0; i < arrTarget.GetLength(0); i++){
            for (int j = 0; j < arrTarget.GetLength(0); j++){
                if (arrTarget[i,j] == true){
@@ -111,18 +103,10 @@ public class BattleSystem : MonoBehaviour
                }
            }
        }
-
-
+       //Player begins!
+       state = BattleState.PLAYERTURN;
        StopCoroutine(PlayerTurn());
     }
-
-    public void PlayerHUDChanges(string text){
-        playerHud.SetPlayerMana(cmang.mana);
-        playerHud.HUDTextUpdate(text);
-        
-    }
-
-
 //Begins the next phase!
     public void EndTurnButton(){
         if (state != BattleState.PLAYERTURN){
@@ -147,57 +131,48 @@ public class BattleSystem : MonoBehaviour
             }
         }
         //Checking to see how many enemies are alive or dead
-        bool isDead = false;
         for (int i = 0; i < pHolderenemy.Count; i++){
             if (pHolderenemy[i].health > 0){
-                isDead = false;
+                //At least one enemy is still alive - continue combat
+                state = BattleState.ENEMYTURN;
                 break;
+            } else {
+                state = BattleState.WON;
             }
-            isDead = true;
         }
-   
-        if (isDead){
-            //END COMBAT
-            state = BattleState.WON;
+        if (state == BattleState.WON){
             EndBattle();
-        } else{
-            //Enemy Turn
-            state = BattleState.ENEMYTURN;
+        } else if (state == BattleState.ENEMYTURN){
             StartCoroutine(EnemyTurn());
         }
-
+        
+            
         yield return new WaitForSecondsRealtime(0f);
+         StopCoroutine(AttackPhase());
     }
 
     public IEnumerator EnemyTurn(){
-        StopCoroutine(AttackPhase());
-        
         //Display message
-        PlayerHUDChanges("Enemy Turn");
-
+         playerHud.HUDTextUpdate("Enemy Turn");
        yield return new WaitForSecondsRealtime(1f);
-
        playerHud.StopTextTurnText();
-
-
-        
+       //Do the enemies chosen action
+       //TODO dynamic enemy sizes
         chosenEnemyAction.CardAction(cmang.board, arrTarget, refGrid);
-        
+        //See if health has changed since attack
         hpbar.StartReduceHealth();
          //  chosenAction.CardAction(cmang.board, 1, 3);
         // chosenAction.CardAction(cmang.board, 2, 3);
-
-        yield return new WaitForSecondsRealtime(1f);
-
+        yield return new WaitForSecondsRealtime(0f);
+        //Destroy indicators
         for (int i = 0; i < gridTargets.Count; i++){
             Destroy(gridTargets[i]);
         }
-
-         
-        //Take damage
+        //Check player's health to see if they can still fight
         if (HealthSystem.GetHealth() <= 0){
             state = BattleState.LOST;
             EndBattle();
+            StopCoroutine(EnemyTurn());
         } else {
             state = BattleState.PLAYERTURN;
       
@@ -209,6 +184,7 @@ public class BattleSystem : MonoBehaviour
 
 
     public void EndBattle(){
+        
         if (state== BattleState.WON){
           playerHud.HUDTextUpdate("You won!");
         } else if  (state == BattleState.LOST){
